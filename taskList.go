94f4e7d7dff3334fb67e31ee841ee46e882df8ed
Juan Jose <juanobando.developer@gmail.com>
2022-12-22 11:53:51 -0500

    creating a task list using structs and their methods

---
+++ taskList.go
@@ -0,0 +1,120 @@
+package main
+
+import "fmt"
+
+// taskList struct
+type taskList struct {
+	tasks []*task // slice of pointers to task struct
+}
+
+// add method
+func (t *taskList) add(task *task) {
+	t.tasks = append(t.tasks, task)
+}
+
+// delete method
+func (t *taskList) delete(task *task) {
+	for i, v := range t.tasks {
+		if v == task {
+			t.tasks = append(t.tasks[:i], t.tasks[i+1:]...)
+		}
+	}
+}
+
+// print method
+func (t *taskList) print() {
+	fmt.Println("FROM TASK LIST:")
+	for _, task := range t.tasks {
+		fmt.Printf("name: %s\ndescription: %s\ndone: %t\n", task.name, task.description, task.done)
+	}
+}
+
+// just done tasks
+func (t *taskList) doneTasks() {
+	fmt.Println("DONE TASKS:")
+	for _, task := range t.tasks {
+		if task.done {
+			fmt.Printf("name: %s\ndescription: %s\ndone: %t\n", task.name, task.description, task.done)
+		}
+	}
+}
+
+// task struct
+type task struct {
+	name        string
+	description string
+	done        bool
+}
+
+// markDone method
+func (t *task) markDone() {
+	t.done = true
+}
+
+// updateDescription method
+func (t *task) updateDescription(description string) {
+	t.description = description
+}
+
+// updateName method
+func (t *task) updateName(name string) {
+	t.name = name
+}
+
+// print method
+func (t *task) print() {
+	fmt.Printf("name: %s\ndescription: %s\ndone: %t\n", t.name, t.description, t.done)
+}
+
+func main() {
+	// create new task
+	myTask := task{name: "My Task", description: "My Task Description", done: false}
+
+	// print task
+	myTask.print()
+
+	// mark task as done
+	myTask.markDone()
+
+	// print task
+	myTask.print()
+
+	// update task description
+	myTask.updateDescription("My Task Description Updated")
+
+	// print task
+	myTask.print()
+
+	// update task name
+	myTask.updateName("My Task Updated")
+
+	// print task
+	myTask.print()
+
+	// another way
+	myTask2 := task{"My Task 2", "My Task Description 2", false}
+	myTask2.print()
+	// myTask2.markDone()
+	myTask2.updateDescription("My Task Description 2 Updated")
+	myTask2.updateName("My Task 2 Updated")
+	myTask2.print()
+
+	// create new task list
+	myTaskList := taskList{tasks: []*task{}}
+
+	// add task to task list
+	myTaskList.add(&myTask)
+	myTaskList.add(&myTask2)
+
+	// print task list
+	myTaskList.print()
+
+	// print done tasks
+	myTaskList.doneTasks()
+
+	// delete task from task list
+	myTaskList.delete(&myTask)
+
+	// print task list
+	myTaskList.print()
+}

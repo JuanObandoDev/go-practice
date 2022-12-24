599cb7ce8bc6753d40e1684e3dc0fc6fd35a1b41
Juan Jose <juanobando.developer@gmail.com>
2022-12-24 15:11:24 -0500

    modify user struct and adding a reciver function to parse it to json

--- webServerProject/types.go
+++ webServerProject/types.go
@@ -1,15 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 // Middleware type
 type Middleware func(http.HandlerFunc) http.HandlerFunc
 
 // User type
 type User struct {
-	Name  string
-	Email string
-	Phone string
+	Name  string `json:"name"` // tag
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+}
+
+func (u *User) ToJSON() ([]byte, error) {
+	return json.Marshal(u)
 }
 
 // MetaData type

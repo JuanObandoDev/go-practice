c29da70bf0b2619477c04b9272cdeefd7239bde5
Juan Jose <juanobando.developer@gmail.com>
2022-12-24 15:12:32 -0500

    creating a user post request function using user struct and toJSON method

--- webServerProject/handlers.go
+++ webServerProject/handlers.go
@@ -27,3 +27,21 @@ func PostRequest(w http.ResponseWriter, req *http.Request) {
 	}
 	fmt.Fprintf(w, "Metadata: %v", metadata)
 }
+
+// userPostRequest handler function
+func UserPostRequest(w http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
+	var user User
+	err := decoder.Decode(&user)
+	if err != nil {
+		fmt.Fprintf(w, "Error: %v", err)
+		return
+	}
+	res, err := user.ToJSON()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
+}

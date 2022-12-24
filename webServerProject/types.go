1b8d5fb7f90099aec556a70e5f4008f0f89932db
Juan Jose <juanobando.developer@gmail.com>
2022-12-24 14:55:50 -0500

    Implementing user type and metadata type

--- webServerProject/types.go
+++ webServerProject/types.go
@@ -4,3 +4,13 @@ import "net/http"
 
 // Middleware type
 type Middleware func(http.HandlerFunc) http.HandlerFunc
+
+// User type
+type User struct {
+	Name  string
+	Email string
+	Phone string
+}
+
+// MetaData type
+type MetaData interface{}
